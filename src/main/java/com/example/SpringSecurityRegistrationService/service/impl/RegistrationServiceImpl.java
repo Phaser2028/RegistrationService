053ae0361848e3f3015cd48631a209709dce951b
Phaser2028 <dima.2000600@gmail.com>
2023-08-30 19:22:11 +0300

    add a class implementing the registration service interface

--- src/main/java/com/example/SpringSecurityRegistrationService/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/example/SpringSecurityRegistrationService/service/impl/RegistrationServiceImpl.java
@@ -1,14 +1,12 @@
 package com.example.SpringSecurityRegistrationService.service.impl;
 
 
-import com.example.SpringSecurityRegistrationService.config.MailProperty;
 import com.example.SpringSecurityRegistrationService.exception.ProfilePictureException;
 import com.example.SpringSecurityRegistrationService.model.Person;
 import com.example.SpringSecurityRegistrationService.repository.PeopleRepository;
 import com.example.SpringSecurityRegistrationService.service.RegistrationService;
 import com.example.SpringSecurityRegistrationService.util.Imager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,50 +23,38 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final PeopleRepository peopleRepository;
     private final Imager imager;
 
+    private final EmailMessageBuilder emailMessageBuilder;
+
     private final JavaMailSender emailSender;
 
-    private final MailProperty mailProperty;
-
-
     @Autowired
-    public RegistrationServiceImpl(PasswordEncoder passwordEncoder, PeopleRepository peopleRepository, Imager imager, JavaMailSender emailSender, MailProperty mailProperty) {
+    public RegistrationServiceImpl(PasswordEncoder passwordEncoder, PeopleRepository peopleRepository, Imager imager, EmailMessageBuilder emailMessageBuilder, JavaMailSender emailSender) {
         this.passwordEncoder = passwordEncoder;
         this.peopleRepository = peopleRepository;
         this.imager = imager;
+        this.emailMessageBuilder = emailMessageBuilder;
         this.emailSender = emailSender;
-        this.mailProperty = mailProperty;
     }
 
 
     @Override
     public void register(Person person, MultipartFile file) {
-
-
         try {
             imager.setProfilePicture(person, file);
         } catch (IOException e) {
             throw new ProfilePictureException("Something went wrong.");
         }
 
-
         person.setPassword(passwordEncoder.encode(person.getPassword()));
         person.setRole("ROLE_USER");
 
         person.setActivationCode(UUID.randomUUID().toString());
 
-        String messageText = "Активация: http://localhost:8080/auth/activate/" + person.getActivationCode();
+
+        this.emailSender.send(emailMessageBuilder.build(person));
 
 
-        SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom(mailProperty.getUsername());
-        System.out.println(mailProperty.getUsername());
-        message.setTo(person.getEmail());
-        message.setSubject("Activation code");
-        message.setText(messageText);
-
-
-        this.emailSender.send(message);
         peopleRepository.save(person);
     }
 
