a9ca8b8ea2aea852e41c063ab562969d8fa1766a
Phaser2028 <dima.2000600@gmail.com>
2023-08-30 19:12:19 +0300

    small changes

--- src/main/java/com/example/SpringSecurityRegistrationService/dto/PersonDTO.java
+++ src/main/java/com/example/SpringSecurityRegistrationService/dto/PersonDTO.java
@@ -1,45 +1,28 @@
 package com.example.SpringSecurityRegistrationService.dto;
 
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
-@Table
-@Data
+
+@Getter
+@Setter
 public class PersonDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
 
-    @Column(name = "username")
-    @NotBlank(message = "Это поле не должно быть пустым")
+    @NotEmpty(message = "Это поле не должно быть пустым")
     private String username;
 
-    @Column(name = "email")
     @Email(message = "Email должен быть валидным")
-    @NotBlank(message = "Это поле не должно быть пустым")
+    @NotEmpty(message = "Это поле не должно быть пустым")
     private String email;
 
-    @Column(name = "activation_code")
     private String activationCode;
 
-    @Column(name = "phone_number")
-    private String phone_number;
-
-    @Column(name = "role")
-    private String role;
-
-    @Column(name = "profile_pic_dir")
-    private String profilePicDir;
-
-    @Column(name = "password")
-    @NotBlank(message = "Это поле не должно быть пустым")
+    @NotEmpty(message = "Это поле не должно быть пустым")
     private String password;
 
-    @Transient
     private String confirmPassword;
+
 }
