482c0574794243a761d099477e8f27a298eff02d
Phaser2028 <dima.2000600@gmail.com>
2023-08-30 19:20:41 +0300

    add an exception for errors related to loading a profile image

---
+++ src/main/java/com/example/SpringSecurityRegistrationService/exception/ProfilePictureException.java
@@ -0,0 +1,8 @@
+package com.example.SpringSecurityRegistrationService.exception;
+
+
+public class ProfilePictureException extends RuntimeException{
+    public ProfilePictureException(String message) {
+        super(message);
+    }
+}
