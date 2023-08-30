246e344da2560f2ead10b10d1d23da677c70ee61
Phaser2028 <dima.2000600@gmail.com>
2023-08-30 19:16:42 +0300

    small changes

---
+++ src/main/java/com/example/SpringSecurityRegistrationService/repository/PeopleRepository.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityRegistrationService.repository;
+
+import com.example.SpringSecurityRegistrationService.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface PeopleRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
+
+    Optional<Person> findByActivationCode(String code);
+}
