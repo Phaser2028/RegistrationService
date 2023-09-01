dc86410fdef524135c16ed9b7c7c7df606e66d80
Dmitriy <dima.2000600@gmail.com>
2023-09-01 16:51:21 +0300

    small changes

--- src/main/java/com/example/SpringSecurityRegistrationService/SpringSecurityRegistrationService.java
+++ src/main/java/com/example/SpringSecurityRegistrationService/SpringSecurityRegistrationService.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecurityRegistrationService;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/SpringSecurityRegistrationService/repository/PeopleRepository.java
+++ src/main/java/com/example/SpringSecurityRegistrationService/repository/PeopleRepository.java
@@ -3,10 +3,11 @@ package com.example.SpringSecurityRegistrationService.repository;
 import com.example.SpringSecurityRegistrationService.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.Optional;
 
 
-public interface PeopleRepository extends JpaRepository<Person, Long> {
+public interface PeopleRepository extends JpaRepository<Person,Long> {
     Optional<Person> findByEmail(String email);
 
     Optional<Person> findByActivationCode(String code);
